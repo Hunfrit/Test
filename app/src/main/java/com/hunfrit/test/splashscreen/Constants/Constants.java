140273aac8bdfd0ab5b7cce0cf20e9df9ab6e40d
Hunfrit <Artem.Hunfrit@gmail.com>
2017-08-16 15:21:04 +0300

    const

--- app/src/main/java/com/hunfrit/test/splashscreen/Constants/Constants.java
+++ app/src/main/java/com/hunfrit/test/splashscreen/Constants/Constants.java
@@ -8,7 +8,7 @@ public final class Constants {
 
     public static final int DIALOG = 1337;
 
-    public static final int SERVER_ERROR_DIALOG = 1488;
+    public static final int SERVER_ERROR_DIALOG = 1402;
 
     public static final String URL = "https://bank.gov.ua/";
 
